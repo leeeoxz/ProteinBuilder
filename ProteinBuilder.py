27a1a4b744258482c0863c1ace227cf6b8aeb7eb
leeeoxz <leonardoas95@gmail.com>
2017-06-26 14:53:55 -0300

    continua fazendo o ortogonal

--- ProteinBuilder.py
+++ ProteinBuilder.py
@@ -1,19 +1,24 @@
 from AADictBuilder import BuildDictionary as BD
-from PDB_read import PDB_read as pdbread
 import copy
+from Rotational import getAngle
+import numpy as np
+import math
 
 class ProteinBuilder():
 	aminoacidDict = None
 	carbTAG = ["OC","HOC","HO","HC"]
 	hTAG = ["1H","H"]
 	seq = None
-	proteinStruct = None
+	proteinStruct = None #array of amino acids, which are arrays of atoms
+	seqSize = None #int value representing the len of the sequence
 
 	def __init__(self,sequence):
 		self.seq = sequence
 		self.aminoacidDict = BD().getDic() #creates a dictionary with all 20 aminoacids and its atom locations
 		self.proteinStruct = []
 		self.setStructure()
+		self.seqSize = len(self.proteinStruct)
+		self.setAngle()
 
 
 	def removeOH(self,last):
@@ -66,5 +71,42 @@ class ProteinBuilder():
 				atom[1] = x
 				x = x +1
 
+	def rotate(self):
+		
+
+	def calcAngle3p(self,ap,cp,pp):
+		ap_mt = np.matrix(ap)
+		cp_mt = np.matrix(cp)
+		pp_mt = np.matrix(pp)
+		v1 = ap_mt - cp_mt
+		v2 = cp_mt - pp_mt
+		v1n= v1/np.linalg.norm(v1)
+		v2n= v2/np.linalg.norm(v2)
+		res = np.sum(np.multiply(v1n,v2n))
+		return math.degrees(float(np.arccos(res)))
+
+	def setAngle(self):
+		for x,aa in enumerate(self.proteinStruct):
+			if (x!=(self.seqSize-1)): #first amino acid
+				for atom in aa:
+					if atom[2] == 'C': #only calculates from C terminal as the anterior position
+						ap = [atom[5],atom[6],atom[7]] #ap = anterior position, [float(Px),float(Py),float(Pz)]
+						for atom in self.proteinStruct[x+1]: #gets the coordinates from the next amino acid
+							if atom[2] == "N": #in this case, the Nitrogen is going to be the central atom
+								cp = [atom[5],atom[6],atom[7]] #cp = central position, [float(Px),float(Py),float(Pz)]
+							elif atom[2] == "2H":  #in this case, the Hydrogen bonded to Nitrogen is going to be the posterial atom
+							    ppH = [atom[5],atom[6],atom[7]] #pp = posteral position, [float(Px),float(Py),float(Pz)]
+							elif atom[2] == "CA": #in this case, the Alpha Carbon bonded to Nitrogen is going to be the posterial atom
+								ppCA = [atom[5],atom[6],atom[7]] #pp = posteral position, [float(Px),float(Py),float(Pz)]
+			elif (x==(self.seqSize-1)):
+				break
+			rotate = 120 - self.calcAngle3p(ap,cp,ppH)
+			print rotate
+
+
+
 	def getStructure (self):
 		return copy.deepcopy(self.proteinStruct)
+
+
+ProteinBuilder("YGGMF")
